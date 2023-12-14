dd39ca73357e98bcb1c6f845d04460cb5b788a4e
Henry Seigbein <hensco95@gmail.com>
2023-12-14 07:37:23 +0100

    added styling for log in page

---
+++ app.js
@@ -0,0 +1,23 @@
+// const url = prompt("enter a url");
+
+// fetch(url)
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((result) => {
+//     console.log("success", result);
+//   })
+//   .catch((status) => {
+//     console.log("error", status);
+//   });
+
+// async function getData(url) {
+//   const response = await fetch(url);
+//   const result = await response.json();
+//   return result;
+// }
+
+// (async () => {
+//   const result = await getData(url);
+//   console.log(result);
+// })();
