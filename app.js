487bbdc1b2aed58b260516a52271219066ad9581
Henry Seigbein <hensco95@gmail.com>
2023-12-15 07:41:20 +0100

    finished styling page, made it accessible using aria

--- app.js
+++ app.js
@@ -1,23 +1,15 @@
-// const url = prompt("enter a url");
+let country_wrap = document.getElementById("country");
 
-// fetch(url)
-//   .then((response) => {
-//     return response.json();
-//   })
-//   .then((result) => {
-//     console.log("success", result);
-//   })
-//   .catch((status) => {
-//     console.log("error", status);
-//   });
 
-// async function getData(url) {
-//   const response = await fetch(url);
-//   const result = await response.json();
-//   return result;
-// }
-
-// (async () => {
-//   const result = await getData(url);
-//   console.log(result);
-// })();
+fetch("countries.json")
+  .then((response) => {
+    return response.json();
+  })
+  .then((countries) => {
+    for (const country of countries) {
+      const option = document.createElement("option");
+      option.value = country;
+      option.textContent = country;
+      country_wrap.appendChild(option)
+    }
+  });
